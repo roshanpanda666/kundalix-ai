c3ddccd8dd7b55b2aa324571fce7ac39aa572ad2
roshan <roshanpanda666@gmail.com>
2026-01-10 13:41:24 +0530

    Enhance PDF generation with safe text handling and improved layout; update user registration form placeholder.

--- app/userregister/page.js
+++ app/userregister/page.js
@@ -152,9 +152,12 @@ export default function RegisterForm() {
             className="w-full mb-4 px-4 py-2 rounded-lg bg-white/5 text-white"
           />
 
+          <label className="text-xs text-white/60 mb-1 block">
+            remember your password !
+          </label>
           <input
             type="password"
-            placeholder="Password"
+            placeholder="register a Password"
             ref={passwordRef}
             required
             className="w-full mb-6 px-4 py-2 rounded-lg bg-white/5 text-white"
